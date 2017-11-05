0205d71b8203739e1a087e1ac41f3827c27db8ef
Michael London <michaelondon6@gmail.com>
2017-11-05 17:01:20 -0500

    first commit

---
+++ src/BasicArrays0.java
@@ -0,0 +1,26 @@
+public class BasicArrays0 {
+
+	public static void main(String[] args) {
+		
+		 int[] number = new int[10];
+		 		
+		 		number[0]= -131;
+		 		number[1]= -131;
+		 		number[2]= -131;
+		 		number[3]= -131;
+		 		number[4]= -131;
+		 		number[5]= -131;
+		 		number[6]= -131;
+		 		number[7]= -131;
+		 		number[8]= -131;
+		 		number[9]= -131;
+		 		
+		 		for(int index=0; index <= (number.length -1 ); index++){
+		 			System.out.println("Slot " + index + " contains " + number[index]);
+		 		}
+		 		
+		 		
+		 	}
+		 
+		 }
+	
